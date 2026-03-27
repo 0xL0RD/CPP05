31076eb282b8e5e2b95945ca80f468417968c54d
Rbs <rubsanch@student.42madrid.com>
2026-03-27 15:19:25 +0100

    tests

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 12:08:29 by rubsanch          #+#    #+#             */
+/*   Updated: 2026/03/27 15:16:12 by rubsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RobotomyRequestForm.hpp"
+#include <cstdlib>
+#include <ctime>
+
+RobotomyRequestForm::RobotomyRequestForm(std::string &target)
+	:	AForm(RR_NAME, RR_SIGN_MIN, RR_EXEC_MIN),
+		_target(target)
+{
+	return ;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other)
+	:	AForm(other),
+		_target(other._target)
+{
+	return ;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(void)
+{
+	return ;
+}
+
+RobotomyRequestForm& RobotomyRequestForm::operator=(const RobotomyRequestForm &lhs)
+{
+	this->AForm::operator=(lhs);
+	return (*this);
+}
+
+void	RobotomyRequestForm::execute_child(Bureaucrat const & executor) const
+{
+	(void) executor;
+	std::cout << "[DRILLING NOISES]" << std::endl;
+	std::cout << "[DRILLING NOISES]" << std::endl;
+	std::cout << "[DRILLING NOISES]" << std::endl;
+	std::srand(std::time(0));
+	if (std::rand() % 2 + 1 == 1)
+			std::cout << this->_target
+			<< " has been robotomized" << std::endl;
+	else
+			std::cout << this->_target
+			<< " robotomy FAILED" << std::endl;
+}
+
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,14 @@
 /*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 09:16:51 by rubsanch          #+#    #+#             */
-/*   Updated: 2026/03/27 10:46:40 by rubsanch         ###   ########.fr       */
+/*   Updated: 2026/03/27 15:18:45 by rubsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 class Bureaucrat;
 class ShrubberyCreationForm;
@@ -22,9 +24,9 @@ int	main(void)
 		std::cout << "1) Good" << std::endl;
 		std::string				formName = "ssForm";
 		ShrubberyCreationForm	f(formName);
-		Bureaucrat				b("John", 3);
 		try
 		{
+			Bureaucrat				b("John", 3);
 			f.beSigned(b);
 			std::cout << "Signed" << std::endl;
 			f.execute(b);
@@ -40,9 +42,9 @@ int	main(void)
 		std::cout << "2) BAD: bad grade for signing" << std::endl;
 		std::string				formName = "ssForm2";
 		ShrubberyCreationForm	f(formName);
-		Bureaucrat				b("UnderLevel", 146);
 		try
 		{
+			Bureaucrat				b("UnderLevel", 146);
 			f.beSigned(b);
 			std::cout << "Signed" << std::endl;
 			f.execute(b);
@@ -58,9 +60,63 @@ int	main(void)
 		std::cout << "3) BAD: bad grade for executing" << std::endl;
 		std::string				formName = "ssForm2";
 		ShrubberyCreationForm	f(formName);
-		Bureaucrat				b("UnderLevel", 138);
 		try
 		{
+			Bureaucrat				b("UnderLevel", 138);
+			f.beSigned(b);
+			std::cout << "Signed" << std::endl;
+			f.execute(b);
+			std::cout << b.getName() << " executed " << f.getName() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		std::cout << std::endl;
+		std::cout << "4) Good PresidentialPardonForm" << std::endl;
+		std::string				formName = "CriminalppForm";
+		PresidentialPardonForm	f(formName);
+		try
+		{
+			Bureaucrat				b("VicePresident", 2);
+			f.beSigned(b);
+			std::cout << "Signed" << std::endl;
+			f.execute(b);
+			std::cout << b.getName() << " executed " << f.getName() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		std::cout << std::endl;
+		std::cout << "5) Robotomizing" << std::endl;
+		std::string				formName = "Person";
+		RobotomyRequestForm		f(formName);
+		try
+		{
+			Bureaucrat				b("Ministry of Robots", 2);
+			f.beSigned(b);
+			std::cout << "Signed" << std::endl;
+			f.execute(b);
+			std::cout << b.getName() << " executed " << f.getName() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		std::cout << std::endl;
+		std::cout << "6) Robotomizing" << std::endl;
+		std::string				formName = "Person";
+		RobotomyRequestForm		f(formName);
+		try
+		{
+			Bureaucrat				b("Ministry of Robots", 0);
 			f.beSigned(b);
 			std::cout << "Signed" << std::endl;
 			f.execute(b);
